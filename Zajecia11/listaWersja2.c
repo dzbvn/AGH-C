8267d73ad74b786d2bf9459b0bed76e2d1676798
szymszon <56302798+szymszon@users.noreply.github.com>
2020-12-10 22:13:30 +0100

    Add files via upload

---
+++ Zajecia11/listaWersja2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+struct Node *head = NULL;
+struct Node *current = NULL;
+
+
+void print(){
+    struct Node* ptr = head;
+
+    while(ptr != NULL){
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+void insert(int data){
+    struct Node* link = (struct Node*) malloc(sizeof(struct Node));
+
+    link->data = data;
+    link->next = head;
+    head = link;
+
+}
+int main(){
+
+    insert(10);
+    print();
+    return 0;
+}
